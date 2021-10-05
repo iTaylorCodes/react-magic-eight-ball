6dd620707a423e303d0be44bc245cc1f8214ede3
IanTaylor <76837421+iTaylorCodes@users.noreply.github.com>
2021-10-05 16:28:14 -0700

    Added color counters

--- src/EightBall.js
+++ src/EightBall.js
@@ -6,15 +6,25 @@ const EightBall = ({ answers }) => {
   const [color, setColor] = useState("black");
   const randIndx = Math.floor(Math.random() * answers.length);
 
+  const [redCounter, setRedCounter] = useState(0);
+  const [greenCounter, setGreenCounter] = useState(0);
+  const [goldenrodCounter, setGoldenrodCounter] = useState(0);
+
   function handleClick() {
     const { msg, color } = answers[randIndx];
     setMsg(msg);
     setColor(color);
+    if (color === "red") setRedCounter(redCounter + 1);
+    if (color === "green") setGreenCounter(greenCounter + 1);
+    if (color === "goldenrod") setGoldenrodCounter(goldenrodCounter + 1);
   }
 
   function reset() {
     setMsg("Think of a Question");
     setColor("black");
+    setRedCounter(0);
+    setGreenCounter(0);
+    setGoldenrodCounter(0);
   }
 
   return (
@@ -25,6 +35,17 @@ const EightBall = ({ answers }) => {
       <button className="EightBall-reset-btn" onClick={reset}>
         Reset
       </button>
+      <div className="EightBall-counter-container">
+        <div className="EightBall-counter" style={{ color: "red" }}>
+          Red Count: <span style={{ fontWeight: 700 }}>{redCounter}</span>
+        </div>
+        <div className="EightBall-counter" style={{ color: "green" }}>
+          Green Count: <span style={{ fontWeight: 700 }}>{greenCounter}</span>
+        </div>
+        <div className="EightBall-counter" style={{ color: "goldenrod" }}>
+          Goldenrod Count: <span style={{ fontWeight: 700 }}>{goldenrodCounter}</span>
+        </div>
+      </div>
     </>
   );
 };
